90a4072ed063063601c95465035200f5de517524
AerinLom <aerinlom1@gmail.com>
2024-08-07 08:43:41 -0700

    Task 3: Lifespan

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,5 @@
+-- List bands with Glam rock as their main style, ranked by their longevity using IF
+SELECT band_name, (IF(split IS NULL, 2022, split) - formed) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
