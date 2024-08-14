49782f257242dc3199b8b7f13e4002c3cdb40cbf
AerinLom <aerinlom1@gmail.com>
2024-08-14 11:49:46 +0000

    Task 1: Original types

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -18,3 +18,23 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self,
+            key: str, fn: Optional[Callable] = None) -> str:
+        """
+        Retrieve data from Redis and apply an optional conversion function.
+        """
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> Optional[str]:
+        """Retrieve a string value from Redis."""
+        return self.get(key, fn=lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """Retrieve an integer value from Redis."""
+        return self.get(key, fn=int)
