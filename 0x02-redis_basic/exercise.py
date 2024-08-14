63d762304cacdb943a411db84b52717abcabd70b
AerinLom <aerinlom1@gmail.com>
2024-08-14 15:46:51 +0000

    Task 4: Caching lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -51,29 +51,23 @@ def replay(fn: Callable) -> None:
     """
     if fn is None or not hasattr(fn, '__self__'):
         return
-
-    cache_memory = getattr(fn.__self__, '_redis', None)
-    if not isinstance(cache_memory, redis.Redis):
+    redis_store = getattr(fn.__self__, '_redis', None)
+    if not isinstance(redis_store, redis.Redis):
         return
-
     fxn_name = fn.__qualname__
-    input_k = '{}:inputs'.format(fxn_name)
-    output_k = '{}:outputs'.format(fxn_name)
-
+    in_key = '{}:inputs'.format(fxn_name)
+    out_key = '{}:outputs'.format(fxn_name)
     fxn_call_count = 0
-    if cache_memory.exists(fxn_name) != 0:
-        fxn_call_count = int(cache_memory.get(fxn_name))
-
+    if redis_store.exists(fxn_name) != 0:
+        fxn_call_count = int(redis_store.get(fxn_name))
     print('{} was called {} times:'.format(fxn_name, fxn_call_count))
-
-    inputs = cache_memory.lrange(input_k, 0, -1)
-    outputs = cache_memory.lrange(output_k, 0, -1)
-
-    for inp, out in zip(inputs, outputs):
+    fxn_inputs = redis_store.lrange(in_key, 0, -1)
+    fxn_outputs = redis_store.lrange(out_key, 0, -1)
+    for fxn_input, fxn_output in zip(fxn_inputs, fxn_outputs):
         print('{}(*{}) -> {}'.format(
             fxn_name,
-            inp.decode('utf-8'),
-            out.decode('utf-8')
+            fxn_input.decode("utf-8"),
+            fxn_output,
         ))
 
 
